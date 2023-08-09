e3bd6e647e271fdc4611701781419888418dd88b
Ibrahim Farooq <ifarooq9@gmail.com>
2023-08-09 12:30:49 +0430

    bugs fixed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express()
 
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://10.8.220.158:8085',
     credentials: true,            //access-control-allow-credentials:true
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
